37be97c2c18b0d01612ed03dcf7e517f1a7f4a26
mwodka <michaelwodka12@gmail.com>
2018-02-19 15:12:24 -0700

    test

---
+++ flasklaunch.py
@@ -0,0 +1,16 @@
+# serve.py
+
+from flask import Flask  
+from flask import render_template
+
+# creates a Flask application, named app
+app = Flask(__name__)
+
+# a route where we will display a welcome message via an HTML template
+@app.route("/")
+def hello():  
+    return render_template('index.html')
+
+# run the application
+if __name__ == "__main__":  
+    app.run()
