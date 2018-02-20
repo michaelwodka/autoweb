abfd60ad157ec30347f4bc0f02ac15714cb11114
mwodka <michaelwodka12@gmail.com>
2018-02-20 10:09:33 -0700

    tester

--- flasklaunch.py
+++ flasklaunch.py
@@ -1,7 +1,7 @@
 # serve.py
-
-from flask import Flask  
+from flask import Flask
 from flask import render_template
+from selenium import webdriver
 
 # creates a Flask application, named app
 app = Flask(__name__)
@@ -10,6 +10,16 @@ app = Flask(__name__)
 @app.route("/")
 def hello():  
     return render_template('index.html')
+
+@app.route('/static/html/start.html')
+def process():
+    options = webdriver.ChromeOptions()
+    options.add_argument("--headless")
+    options.add_argument("--no-sandbox")
+    options.add_argument('/autowebform/.apt/usr/bin/google-chrome')
+    driver = webdriver.Chrome(executable_path='/autowebform/.chromedriver/bin/chromedriver', chrome_options=options)
+    driver.get("https://www.google.com")
+    driver.save_screenshot("screenshot.png")
 
 # run the application
 if __name__ == "__main__":  
