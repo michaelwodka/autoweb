27c09f0d15488635cad8befd9a7f02930a50ba56
mwodka <michaelwodka12@gmail.com>
2018-02-20 16:46:56 -0700

    test

--- flasklaunch.py
+++ flasklaunch.py
@@ -2,6 +2,7 @@
 from flask import Flask
 from flask import render_template
 from selenium import webdriver
+import os
 
 # creates a Flask application, named app
 app = Flask(__name__)
@@ -13,13 +14,17 @@ def hello():
 
 @app.route('/static/html/start.html')
 def process():
-    options = webdriver.ChromeOptions()
-    options.add_argument("--headless")
-    options.add_argument("--no-sandbox")
-    options.add_argument('/autowebform/.apt/usr/bin/google-chrome')
-    driver = webdriver.Chrome(executable_path='/autowebform/.chromedriver/bin/chromedriver', chrome_options=options)
-    driver.get("https://www.google.com")
-    driver.save_screenshot("screenshot.png")
+    chrome_bin = os.environ.get('GOOGLE_CHROME_SHIM', None)
+    opts = webdriver.ChromeOptions()
+    opts.binary_location = chrome_bin
+    driver = webdriver.Chrome(executable_path="chromedriver", chrome_options=opts)
+    driver.get('https://michaelwodka.com/')
+    driver.find_element_by_name("name").send_keys("hi")
+    driver.find_element_by_id("email").send_keys("msw233@cornell.edu")
+    driver.find_element_by_name("_subject").send_keys("hi")
+    driver.find_element_by_name("message").send_keys("hi")
+    driver.find_element_by_xpath('/html/body/section[4]/div/div/form/div[5]/div/button').click()
+    return driver.page_source
 
 # run the application
 if __name__ == "__main__":  
